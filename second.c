256ad95d92d3aed5597040e3a3d8feb52f3c6ce2
kishorgyawali97 <kishorgyawali43@gmail.com>
2025-01-07 15:47:07 +0545

    Second commit. Just Learning Git

---
+++ second.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main(void){
+
+    int a,b;
+    printf("Enter any two numbers\n ");
+    scanf("%d %d", &a, &b);
+    printf("The sum is %d\n", a+b);
+    return 0;
+}
+
